a33752fa0a4e2ddccbd4c45f411b1acf56c334d7
Darlan Marcelino da Silva <107887900+Darlanmarcelinodasilva@users.noreply.github.com>
2023-03-20 17:25:53 -0300

    desafios

---
+++ Desafios/006.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite um número:'))
+dobro = n1 * 2
+triplo = n1 * 3
+ra = n1 * n1
+print('O dobro de', n1, 'é', dobro, 'o triplo é',
+      triplo, 'e sua raiz quadrada é:', ra)
---
+++ Desafios/007.py
@@ -0,0 +1,4 @@
+nota01 = float(input('Digite a primeira nota:'))
+nota02 = float(input('Digite a segunda nota:'))
+media = (nota01 + nota02) / 2
+print('A média do aluno(a) é: {}'.format(media))
