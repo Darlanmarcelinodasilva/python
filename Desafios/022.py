86a99089915d3ce5f99ed3d8ae77fd5400a575d3
Darlan Marcelino da Silva <107887900+Darlanmarcelinodasilva@users.noreply.github.com>
2023-03-22 14:54:37 -0300

    update

---
+++ Desafios/022.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# => O nome de todos com letras maiúsculas
+# => O nome de todos em minúsculas
+# => Quantas letras ao todo (sem considerar espaços)
+# => Quantas letras tem o primeiro nome
---
+++ aula09.py
@@ -0,0 +1,6 @@
+# Manipulando Texto
+frase = 'Curso em Vídeo Python'
+print(frase[:13])
+print(frase.split())
+dividido = frase.split()
+print(dividido[2][3])
