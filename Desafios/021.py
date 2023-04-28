23be61aa4a048cd1f976391396475c5e0b7eea5a
Darlan Marcelino da Silva <107887900+Darlanmarcelinodasilva@users.noreply.github.com>
2023-04-28 11:38:00 -0300

    ok

--- Desafios/021.py
+++ Desafios/021.py
@@ -1,5 +1,12 @@
+#ou
+#import playsound
+#playsound.playsound('python/Desafios/02.mp3')
+
 import pygame
-pygame.init()
-pygame.mixer.music.load('01.mp3')
+pygame.mixer.init()
+pygame.mixer.music.load('python/Desafios/02.mp3')
 pygame.mixer.music.play()
+input()
 pygame.event.wait()
+#Com isso, você consegue digitar o nome do arquivo e tocar ele(contanto que ele esteja contido na pasta do seu código!)
+#fica a dica!
--- Desafios/022.py
+++ Desafios/022.py
@@ -1,5 +1,12 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print('O seu nome em MAIÚSCULAS é {}'.format(nome.upper()))
+print('O seu nome em minúsculas é {}'.format(nome.lower()))
+print('Seu nome possui {} letras'.format(len(nome) - nome.count(' ')))
+# ou print ('O seu primeiro nome tem ao todo {} letras'.format(nome.find(' '))
+
 # Crie um programa que leia o nome completo de uma pessoa e mostre:
-# => O nome de todos com letras maiúsculas
-# => O nome de todos em minúsculas
+# => O nome de todo com letras maiúsculas
+# => O nome de todo em minúsculas
 # => Quantas letras ao todo (sem considerar espaços)
 # => Quantas letras tem o primeiro nome
